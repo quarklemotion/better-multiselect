5e2f8fe79772588a8297d319207ac0892afde197
Michael Fields <mfields1@gmail.com>
2013-11-01 00:50:22 -0400

    change to use simpler onLoad event injection

--- js/better-multiselect.js
+++ js/better-multiselect.js
@@ -1,49 +1,3 @@
-/*    EventCache Version 1.0
-    Copyright 2005 Mark Wubben
-
-    Provides a way for automagically removing events from nodes and thus preventing memory leakage.
-    See </web/20060501083526/http://novemberborn.net/javascript/event-cache> for more information.
-    
-    This software is licensed under the CC-GNU LGPL </web/20060501083526/http://creativecommons.org/licenses/LGPL/2.1/>
-*/
-
-/*    Event Cache uses an anonymous function to create a hidden scope chain.
-    This is to prevent scoping issues. */
-var EventCache = function(){
-    var listEvents = [];
-    
-    return {
-        listEvents : listEvents,
-    
-        add : function(node, sEventName, fHandler, bCapture){
-            listEvents.push(arguments);
-        },
-    
-        flush : function(){
-            var i, item;
-            for(i = listEvents.length - 1; i >= 0; i = i - 1){
-                item = listEvents[i];
-                
-                if(item[0].removeEventListener){
-                    item[0].removeEventListener(item[1], item[2], item[3]);
-                };
-                
-                /* From this point on we need the event names to be prefixed with 'on" */
-                if(item[1].substring(0, 2) != "on"){
-                    item[1] = "on" + item[1];
-                };
-                
-                if(item[0].detachEvent){
-                    item[0].detachEvent(item[1], item[2]);
-                };
-                
-                item[0][item[1]] = null;
-            };
-        }
-    };
-}();
-
-
 /*
   Better-Multiselect Javascript Library
   Converts all standard multiselects on the HTML page to DHTML versions that do not require control-clicks for
@@ -217,21 +171,20 @@ var betterMultiselect = {
         elem.parentNode.replaceChild(outerDiv, elem);
         return true;
     },
-    addEvent: function( obj, type, fn ) {
-      if (obj.addEventListener) {
-        obj.addEventListener( type, fn, false );
-        EventCache.add(obj, type, fn);
-      }
-      else if (obj.attachEvent) {
-        obj["e" + type + fn] = fn;
-        obj[type + fn] = function() { obj["e" + type + fn]( window.event ); }
-        obj.attachEvent( "on" + type, obj[type + fn] );
-        EventCache.add(obj, type, fn);
-      }
-      else {
-        obj["on" + type] = obj["e" + type + fn];
-      }
-    }    
+
+    addLoadEvent: function(func) {
+        var oldonload = window.onload;
+        if (typeof window.onload != 'function') {
+            window.onload = func;
+        } else {
+            window.onload = function() {
+                if (oldonload) {
+                    oldonload();
+                }
+                func();
+            }
+        }
+    }
 };
 
-betterMultiselect.addEvent(window,'load', betterMultiselect.convertFormElements);
+betterMultiselect.addLoadEvent(betterMultiselect.convertFormElements);
